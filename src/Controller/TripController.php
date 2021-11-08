f521329ff05fdaee89a3129c063f96c5aa30f8f0
SteevenT <tillier.steeven36@gmail.com>
2021-11-08 15:44:02 +0100

    Update TripController.php

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -6,8 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Trip;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Repository\TripRepository;
+
 class TripController extends AbstractController
 {
+    /**
+     * @Route("/home", name="creation_sortie")
+     */
+    public function index(TripRepository $tripRepo, UserRepository $userRepo): Response
+    {
+        $trips = $tripRepo->findAll();
+        $authUser = $this->getUser();
+
+        return $this->render('trip/index.html.twig', [
+            'trips' => $trips,
+            'user' => $authUser,
+        ]);
+    }
+
     /**
      * @Route("/creationSortie", name="creation_sortie")
      */
